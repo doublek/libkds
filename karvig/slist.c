56a221f8b07ccefea8873cee90e6d0b143a416ae
doublek <karthikkrishnan.r@gmail.com>
2010-12-27 02:26:21 -0800

    Prepare for insert/delete at front and end.
    Modify slist in preparation for inserting and deleting from front or
    back. Right now only operations from the front is supported. Support for
    operations from the end coming soon.

--- karvig/slist.c
+++ karvig/slist.c
@@ -59,7 +59,44 @@ slist_t * slist_init(void)
     return slist;
 }
 
+/*
+ * slist_insert_first_node() - Insert first node into a slist.
+ *
+ */
+int slist_insert_first_node(slist_t **slist, void *data)
+{
+    slist_node_t *new_node;
+    new_node = malloc(sizeof(slist_node_t));
+
+    if (new_node == NULL)
+        return ENOMEM;
+
+    printf("Creating first element in the list with data %s\n",
+            (char *)data);
+    new_node->data = data;
+    new_node->next = NULL;
+    (*slist)->head = new_node;
+    return 0;
+
+}
+
+/*
+ * slist_insert() - Default insert operation.
+ *
+ * Inserts data at front. A simple wrapper that just calls
+ * slist_insert_at_front().
+ */
 int slist_insert(slist_t **slist, void *data)
+{
+    return slist_insert_at_front(slist, data);
+}
+
+/*
+ * slist_insert_at_front() - Default insert operation.
+ *
+ * Inserts data at the front of the list.
+ */
+int slist_insert_at_front(slist_t **slist, void *data)
 {
     slist_node_t *new_node;
     new_node = malloc(sizeof(slist_node_t));
@@ -70,11 +107,7 @@ int slist_insert(slist_t **slist, void *data)
     /* Inserting the first element */
     if ((*slist)->head == NULL)
     {
-        printf("Creating first element in the list with data %s\n", (char *)data);
-        new_node->data = data;
-        new_node->next = NULL;
-        (*slist)->head = new_node;
-        return 0;
+        return slist_insert_first_node(slist, data);
     }
 
     printf("Inserting %s in the list\n", (char *)data);
@@ -86,10 +119,21 @@ int slist_insert(slist_t **slist, void *data)
 }
 
 /*
- * slist_remove() - Remove from head entry.
+ * slist_delete() - Default delete operation.
  *
+ * Deletes a node from the list. This is just a simple wrapper around the 
+ * slist_delete_at_front() method.
  */
 int slist_delete(slist_t **slist)
+{
+    return slist_delete_at_front(slist);
+}
+
+/*
+ * slist_remove_at_front() - Remove from head entry.
+ *
+ */
+int slist_delete_at_front(slist_t **slist)
 {
     slist_node_t *node_to_delete;
     
--- karvig/slist.h
+++ karvig/slist.h
@@ -1,13 +1,18 @@
 #ifndef KARVIG_INCLUDE_SLIST_H_
 #define KARVIG_INCLUDE_SLIST_H_
 
-/* #include <inttype.h> */
 #include "localmacros.h"
 
-/* The node of singly pointed list data structure*/
+/* The node of singly pointed list data structure.
+ * XXX Note that we current;y use a void* data type below. Although this is
+ * present to allow generic data types to be used, it is recommended to not
+ * mix data types while using the slist. slist should still work with a
+ * mixture of data types but it is highly recommended not to do so for
+ * clarity as well as to ensure less error prone programs.
+ */
 typedef struct _slist_node_t {
     struct _slist_node_t *next;
-    void *data;         /* How to handle multiple data. Will a cast do? */
+    void *data;
 }slist_node_t;
 
 /* The actual singly pointed list data structure*/
@@ -20,10 +25,11 @@ __BEGIN_DECLS
 
 slist_t *   slist_init();
 int         slist_insert(slist_t **slist, void *data);
+int         slist_insert_at_front(slist_t **slist, void *data);
 int         slist_delete(slist_t **slist);
+int         slist_delete_at_front(slist_t **slist);
 /*
 extern int          slist_insert_at_end(slist_t **slist, void *data);
-extern int          slist_insert_in_front(slist_t **slist, void *data);
 extern int          slist_delete_at_end(slist_t **slist, void *data);
 extern int          slist_delete_in_front(slist_t **slist, void *data);
 */
