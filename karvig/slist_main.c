76cef659ec4e98563ed0fb0e1bdd5054b008c373
doublek <karthikkrishnan.r@gmail.com>
2010-12-27 01:30:48 -0800

    Modify tests.
    Ensure that we only add pointers to the data to the list. This way, we
    can store any data type to slist. At present even a mixture of data
    works although it is not recommended to be used that way.

--- karvig/slist_main.c
+++ karvig/slist_main.c
@@ -5,9 +5,44 @@
 
 #include "slist.h"
 
+void print_list(slist_t *slist)
+{
+    slist_node_t *current;
+    char *data_to_print;
+    int *int_data_to_print;
+    int count = 0;
+    
+    printf("Printing List:\n");
+    current = slist->head;
+    while (current != NULL)
+    {
+        if (count != 0)
+        {
+            data_to_print = (char *)current->data;
+            printf("S %s\n", (char *)data_to_print);
+        }
+        else
+        {
+            int_data_to_print = (int *)current->data;
+            printf("N %d\n", *int_data_to_print);
+
+        }
+        current = current->next;
+        ++count;
+    }
+
+}
+
 int main(int argc, char* argv[])
 {
-    struct slist_t *slist;
+    slist_t *slist;
+
+    int num_data[] = {1, 2, 3, 4};
+    char *text_data[5] = {"A", "B", "C", "D", "E"};
+
+    char *data0, *data1, *data2;
+    int *numeric_test_data;
+
     slist = slist_init();
 
     if(slist == NULL)
@@ -16,33 +51,18 @@ int main(int argc, char* argv[])
     if (slist->head == NULL)
         printf("Head is initialized to NULL\n");
 
-    slist_insert(&slist, (void *)"A");
-    slist_insert(&slist, (void *)"B");
-    slist_insert(&slist, (void *)"C");
-    slist_insert(&slist, (void *)"D");
-    slist_insert(&slist, (void *)"E");
-    slist_insert(&slist, (void *)"F");
-    slist_insert(&slist, (void *)"G");
-    slist_insert(&slist, (void *)"H");
-    slist_insert(&slist, (void *)"I");
-    slist_insert(&slist, (void *)"J");
-    slist_insert(&slist, (void *)"K");
-    slist_insert(&slist, (void *)"L");
-    slist_insert(&slist, (void *)"M");
-    slist_insert(&slist, (void *)"N");
-    slist_insert(&slist, (void *)"O");
-    slist_insert(&slist, (void *)"P");
-    slist_insert(&slist, (void *)"Q");
-    slist_insert(&slist, (void *)"R");
-    slist_insert(&slist, (void *)"S");
-    slist_insert(&slist, (void *)"T");
-    slist_insert(&slist, (void *)"U");
-    slist_insert(&slist, (void *)"V");
-    slist_insert(&slist, (void *)"W");
-    slist_insert(&slist, (void *)"X");
-    slist_insert(&slist, (void *)"Y");
-    slist_insert(&slist, (void *)1);
-    slist_insert(&slist, (void *)"Z");
+
+    data0 = malloc(sizeof(text_data[0]));
+    strncpy(data0, text_data[0], strlen(text_data[0]));
+    slist_insert(&slist, (void *)data0);
+
+    data1 = malloc(sizeof(text_data[1]));
+    strncpy(data1, text_data[1], strlen(text_data[1]));
+    slist_insert(&slist, (void *)data1);
+
+    numeric_test_data = malloc(sizeof(num_data[0]));
+    *numeric_test_data = num_data[0];
+    slist_insert(&slist, (void *)numeric_test_data);
 
     print_list(slist);
     slist_delete(&slist);
